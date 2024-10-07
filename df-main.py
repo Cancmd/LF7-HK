e393d72feb85cd74c7c9a28ee1b0c113936996d3
Rawcat_IND <rawcat@milkno.com>
2024-10-07 10:33:28 +0200

    implemented json parser to correctly return the "dominant_emotion" value

--- df-main.py
+++ df-main.py
@@ -3,6 +3,7 @@ import cv2                      # Library for working with imagefiles
 import numpy as np              # Library for image conversion (the Image is converted into an array and decoded before being referenced by DeepFace)
 from deepface import DeepFace   # Python Wrapper with a large variety of modules for machine learning facial recognition
 import mysql.connector          # Module for connecting the script to the Database
+import json                     # Module to interprete json arrays
 
 # Register the SMB client session | This section needs to be updated!!!
 smbclient.register_session(server="-", username="-", password="-")
@@ -21,9 +22,14 @@ def analyze_image_from_smb(smb_path):
     image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
     
     # Call Deepface to analyze the image. The "actions" parameter narrows down the response body to the one atribute we want
-    results = DeepFace.analyze(img_path=image, actions=['dominant_emotion'])
+    results = DeepFace.analyze(img_path=image, actions=['emotion'])
     
-    return results
+    # Since we cant cant specify which of the emotion values we want we need to first turn the list into a json array
+    jarray = json.dumps(results)
+    # The json array then needs to be parsed
+    jarrayparsed = json.loads(jarray)
+    # In the return value of the function we can specify which value we need
+    return jarrayparsed[0]["dominant_emotion"]
 
 # Specify the SMB path to the .jpg file | This part needs to be reworked with either a directory scan and variable filenames or to run reguarly with the same filename (old ones would get overwriten)
 smb_image_path = r"\\192.168.178.65\imgpool\image1.jpg"
