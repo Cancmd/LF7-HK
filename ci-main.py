b9f6337b2846c6f5eed6af2e736d2561a409d663
Rawcat_IND <rawcat@milkno.com>
2024-10-13 17:53:54 +0200

    built BLOB conversion and DB insertion. WIP

--- ci-main.py
+++ ci-main.py
@@ -1,19 +1,15 @@
 import subprocess
-import smbclient                # Handles the network drive 
-from config import smb_config   # import the smb credentials from config.py
+from config import db_config   # import the db credentials from config.py
 from time import sleep
+import mysql.connector
 
-# Register the SMB client session
-smbclient.register_session(**smb_config)
-# Specify the SMB path to the .jpg file | This part needs to be reworked with either a directory scan and variable filenames or to run reguarly with the same filename (old ones would get overwriten)
-smb_image_path = r"\\192.168.178.65\imgpool\image1.jpg"
-
+output_path = f"/home/PI01/Desktop/Photo/image.jpg"
 def capture_image():
     
     # Command to capture the image with libcamera-still
     command = [
         "libcamera-still", 
-        "-o", smb_image_path,  # Output file with unique name
+        "-o", output_path,  # Output file 
         "--width", "1024",  # Resolution width
         "--height", "768",  # Resolution height
         "--brightness", "0.5"  # Adjust brightness if needed
@@ -25,7 +21,41 @@ def capture_image():
     
     # Capture the image by running the libcamera-still command
     subprocess.run(command)
-    print(f"Image captured and saved to {smb_image_path}")
+    print(f"Image captured and saved to {output_path}")
+
+def binaryconversion(filename):
+    with open(filename, 'rb') as file:
+        binaryfile = file.read()
+    return binaryfile
+
+
+# Function to connect to the database and to create an object
+def insert_into_database(capturejpg):
+    # Connect to the server specified in config.py. This is done so that the connection credentials arent uploaded in the main code
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+
+    # SQL querry to insert values that werent specified yet into the table "emotions" with the collumn "img"
+    query = """
+    INSERT INTO emotions (img)
+    VALUES (%s)
+    """
+    binaryimgfile = binaryconversion(capturejpg)
+
+    # Execute the querry specified previously with the payload
+    cursor.execute(query, (binaryimgfile))
+    
+    # Commit the transaction
+    conn.commit()
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
+    # Print the result
+    print(f"BLOB inserted")
 
 # Call the capture function
 capture_image()
+sleep(1)
+insert_into_database("/home/PI01/Desktop/Photo/image.jpg")
